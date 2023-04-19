d9fd19089431e12ab4c0b008bcb2d7f711de0cfd
MarioBlasi <mblasi509@gmail.com>
2023-04-19 11:58:01 +0100

    structure del global state

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
 <script>
+import AppHeader from "./components/AppHeader.vue";
+import AppMain from "./components/AppMain.vue";
+// import AppCard from "./components/AppCard.vue";
+// import axios from "axios";
 export default {
+  name: "Appvue",
+  components: {
+    AppHeader,
+    AppMain,
+    // AppCard,
+  },
   data() {
-    return {};
+    return {
+      // url: "https://api.themoviedb.org/3/search/movie",
+      // typeSearch: "search/movie",
+      // typeSearchTv: "search/tv",
+      // apiKey: "?api_key=f4a2ba11f237cc06a01698ccbd4cb0f5&query=avatar",
+    };
   },
 };
 </script>
 
 <template>
-  <h1>ciao</h1>
-</template>
+  <AppHeader></AppHeader>
+  <div><h1>AppVue</h1></div>
+  <AppMain></AppMain>
 
+  <!--<AppCard></AppCard> -->
+</template>
+//
+https://api.themoviedb.org/3/search/movie?api_key=f4a2ba11f237cc06a01698ccbd4cb0f5&query=avatar
 <style lang="scss"></style>
---
+++ src/components/AppHeader.vue
@@ -0,0 +1,32 @@
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {};
+  },
+};
+</script>
+
+<template>
+  <header>
+    <div class="container">
+      <div class="row">
+        <div class="col">
+          <div class="SearchBox">
+            <!-- dobbiamo avere accesso a data -->
+            <div class="input-group mb-3">
+              <input
+                type="text"
+                class="form-control"
+                placeholder=" search something"
+              />
+              <button class="btn btn-info" type="button">Search</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </header>
+</template>
+
+<style lang="scss"></style>
