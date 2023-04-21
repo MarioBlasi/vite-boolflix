c9dc22da408c77e1d1935ea9a6767563017b8730
MarioBlasi <mblasi509@gmail.com>
2023-04-21 19:29:44 +0100

    style struttura  scss

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -5,21 +5,14 @@ export default {
   name: "AppHeader",
   data() {
     return {
-     
-      
+      baseUri: "https://api.themoviedb.org/3/",
+      typeSearch: "search/movie",
+      typeSearchTv: "search/tv",
+      apiKey: "?api_key=f4a2ba11f237cc06a01698ccbd4cb0f5",
       store,
-     
     };
   },
   methods: {
-    stelle(index, voto) {
-      const media = Math.ceil(voto / 2);
-      if (media > index) {
-        return true;
-      }
-      console.log(this.stelle);
-    },
-
     performSearch() {
       // film
       axios
@@ -27,9 +20,9 @@ export default {
           `${this.baseUri}${this.typeSearch}${this.apiKey}&query=${this.store.searchText}`
         )
         .then((res) => {
-          this.rispApi = res.data.results;
-          console.log(this.rispApi);
-          if (this.rispApi) this.$emit("headerApi", this.rispApi);
+          this.store.rispApi = res.data.results;
+          console.log(this.store.rispApi);
+          // if (this.rispApi) this.$emit("headerApi", this.rispApi);
         });
       // serie TV
       axios
@@ -37,9 +30,9 @@ export default {
           `${this.baseUri}${this.typeSearchTv}${this.apiKey}&query=${this.store.searchText}`
         )
         .then((res) => {
-          this.rispApiTv = res.data.results;
-          console.log(this.rispApiTv);
-          if (this.rispApiTv) this.$emit("headerApiTv", this.rispApiTv);
+          this.store.rispApiTv = res.data.results;
+          console.log(this.store.rispApiTv);
+          // if (this.rispApiTv) this.$emit("headerApiTv", this.rispApiTv);
           //this.userText = "";
         });
     },
@@ -52,7 +45,7 @@ export default {
     <div class="flex-container" id="logo-search">
       <div class="row list box-card">
         <!-- HEADER -->
-        <div class="col top ">
+        <div class="col top">
           <div class="logoBox d-flex justify-content-between">
             <img
               class="logo p-2 ms-3"
@@ -87,74 +80,6 @@ export default {
               </div>
             </div>
           </div>
-          <!--  CARD -->
-          <div class="card boxcard">
-            <div class="col bottom">
-              <!-- film -->
-              <div class="moviesList" v-if="rispApi.length > 0">
-               <h2>Film</h2>
-                <p>
-                  <p class="poster"  v-for="result in rispApi" :key="result.id">
-                    <img
-                      v-if="result.poster_path"
-                      :src="`https://image.tmdb.org/t/p/w342${result.poster_path}`"
-                    />
-                    <div class="d-none box-info">
-                      <h6 class="copertina">Titolo: {{ result.title }}</h6>
-                      <h6>Titolo Originale: {{ result.original_title }}</h6>
-                      <p class="bandiera">
-                        Lingua:
-                        <img
-                          class="liBandiere"
-                          :src="linguaBandiere[result.original_language]"
-                          height="20px"
-                        />
-                        {{ result.original_language }}
-                      </p>
-                      <div class="voti">
-                        <i
-                          v-for="(star, index) in 5"
-                          :key="index"
-                          class="fas fa-star"
-                          :class="
-                            stelle(index, result.vote_average) ? 'stars' : ''
-                          "
-                        ></i>
-                      </div>
-                      <p class="avarage">Voto: {{ result.vote_average }}</p>
-                      <h6 class="overflow-hidden">{{ result.overview }}</h6>
-                    </div>
-                  </p>
-                </p>
-              </div>
-              <!-- serie Tv -->
-              <div class="moviesList" v-if="rispApiTv.length > 0">
-                <h2>Serie Tv</h2>
-                <p>
-                  <p class="poster" v-for="result in rispApiTv" :key="result.id">
-                    <img
-                      class="liBandiere"
-                      v-if="result.poster_path"
-                      :src="`https://image.tmdb.org/t/p/w342${result.poster_path}`"
-                    />
-                    <div class="d-none box-info">
-                      <h4>Titolo: {{ result.name }}</h4>
-                      <h6>Titolo Originale: {{ result.original_name }}</h6>
-                      <p class="bandiera">
-                        Lingua:
-                        <img
-                          :src="linguaBandiere[result.original_language]"
-                          height="20px"
-                        />
-                        {{ result.original_language }}
-                      </p>
-                      <p>Voto: {{ result.vote_average }}</p>
-                    </div>
-                  </p>
-                </p>
-              </div>
-            </div>
-          </div>
         </div>
       </div>
     </div>
@@ -162,57 +87,6 @@ export default {
 </template>
 
 <style lang="scss">
-.sectionCard{
-  background-color: rgba(27, 27, 27, 0.897);
-  border-radius: 10px;
-}
-.col{
-  background-color: rgba(27, 27, 27, 0.897);
-  border-radius: 10px;
-}
-.boxcard {
-  border-radius: 10px;
-  margin-left: 1rem;
-  border: none;
-
-  .poster > img {
-    border-radius: 10px;
-  }
-  &:hover {
-    .dai > img {
-      display: none;
-    }
-    .box-info {
-      display:block !important;
-    }
-  }
-}
-#logo-search {
-  font-size: medium;
-  p {
-    background-color: rgba(27, 27, 27, 0.897);
-    color: wheat;
-  }
-}
-.stars {
-  color: gold;
-}
-.top {
-  line-height: 30px;
-  .bandiera > img {
-    height: 30px;
-    width: 30px;
-    margin: 0;
-    border-radius: 50%;
-  }
-  h1 {
-    padding: 0 1px;
-  }
-}
-.moviesList h2{
-  color: wheat;
-  background-color:  rgba(27, 27, 27, 0.425);
-}
 .link {
   ul {
     display: flex;
